64d8f42b5261fde57fa0e14d36a2044a64684434
CdP-Antoine <116091736+CdP-Antoine@users.noreply.github.com>
2024-03-04 21:21:59 +0100

    passage sur la tour

---
+++ README.md
@@ -0,0 +1,26 @@
+# Coniguration
+1) .bashrc
+Add this line in your .bashrc 
+`source projects-installers/alias.sh`
+Pay attention to change the path accordingly to your local clone of the repository
+
+2) config.json
+
+# Usage
+This package can install pre-configured ready-to-use projects
+
+The main command to use the package is 
+`projects-installers`
+But you can use the shortcut
+`pi`
+## Wordpress
+Using the following command, you create a wordpress reposity with a full ready-to-use docker stack with :
+- phpmyadmin
+- mariadb
+- wordpress
+On the latest versions each time you use this command :
+`projects-installers wordpress my-wordpress-project`
+
+### Options
+Several flags are available :
+
--- alias.sh
+++ alias.sh
@@ -1,9 +1,15 @@
 echo -e "namespace \e[33m\"projects-installers\"\e[0m has been loaded ; Type\e[36m \"projects-installers help\" \e[0mto display all the available commands \n";
-
-proejcts-installers() {
+scriptDirectory=$(dirname "${BASH_SOURCE[0]}")
+projects-installers() {
     case $1 in 
         "wordpress" | "wp")
-            echo "Installing WP project type"
+            if [ -z "$2" ]; then
+                echo "Expected a project name"
+                return 1
+            fi
+            projectDirectory=$(grep -E '"projectDirectory"\s*:' $scriptDirectory/config.json | sed 's/.*: *"\(.*\)".*/\1/')
+            whereToInstall="$projectDirectory/$2"
+            echo "Installing WP project type in $whereToInstall"
             ;;
         "second")
             echo "second case"
