40c628e46636fa7ca83e94dcd56a15df42ff7b34
AntoineP83 <antoinepawlowski.fr@gmail.com>
2024-03-09 16:21:37 +0100

    Initialisation terminée

--- alias.sh
+++ alias.sh
@@ -1,5 +1,9 @@
 echo -e "namespace \e[33m\"projects-installers\"\e[0m has been loaded ; Type\e[36m \"projects-installers help\" \e[0mto display all the available commands \n";
 scriptDirectory=$(dirname "${BASH_SOURCE[0]}")
+
+scriptRealDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo -e "Le script se trouve dans le répertoire : $scriptRealDirectory"
 projects-installers() {
     case $1 in 
         "wordpress" | "wp")
@@ -7,16 +11,19 @@ projects-installers() {
                 echo "Expected a project name"
                 return 1
             fi
-            projectDirectory=$(grep -E '"projectDirectory"\s*:' $scriptDirectory/config.json | sed 's/.*: *"\(.*\)".*/\1/')
-            whereToInstall="$projectDirectory/$2"
-            echo "Installing WP project type in $whereToInstall"
+            echo "$(pwd)"
+            echo "Installing WP project type in $2"
+            mkdir $2
+            cp "$scriptRealDirectory/wordpress/docker-compose.yml" $2
+            echo "WP project successfully initialized"
+
             ;;
         "second")
             echo "second case"
             ;;
         "third" | "fourth")
             echo "third or fourth case"
-            ;; 
+            ;;
         *)
             echo "other cases"
             ;;
